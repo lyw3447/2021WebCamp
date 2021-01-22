8b209eb438c303241c97fd224a8436c1c6acbfc3
lyw3447 <lyw3447@naver.com>
2021-01-22 10:59:01 +0900

    memberServiceImpl update

--- 2021WebCamp/src/main/java/com/mycompany/myapp/member/MemberServiceImpl.java
+++ 2021WebCamp/src/main/java/com/mycompany/myapp/member/MemberServiceImpl.java
@@ -1,5 +1,14 @@
 package com.mycompany.myapp.member;
 
-public class MemberServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+public class MemberServiceImpl {
+	@Autowired
+	MemberDAO memberDAO;
+	
+	public MemberVO getMember(MemberVO vo) {
+		return memberDAO.getMember(vo);
+	}
 }
